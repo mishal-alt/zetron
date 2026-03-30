bdbca2ad735757a6ebed41fa102ab6eed379b519
MISHAL <mishalkvmishal@gmail.com>
2026-03-30 15:35:18 +0530

    fixed the error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Menu, X,  } from 'lucide-react';
+import { Menu, Rocket, X } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Navbar = () => {
